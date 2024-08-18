5655b5d3a26e98c89d5fc651775fd4ce8ec534f1
piroj <p1r0j@pm.me>
2024-08-17 21:43:04 -0400

    Added remove entry function to tLink.

--- tBook/tLink/tLink.sh
+++ tBook/tLink/tLink.sh
@@ -31,6 +31,22 @@ tLi_add_entry() {
 }
 
 
+# Remove link entry.
+tLi_remove_entry() {
+  if [ -z "$2" ]; then
+    echo "$fEMPTY"
+    echo "$fERROR ${sBGREEN}tLink ${sBBLUE}remove entry${sRESET} requires a target."
+  elif [ ! -f "$edLI/$2" ]; then
+    echo "$fEMPTY"
+    echo "$fERROR ${sHL}$2${sRESET} is not a valid target."
+  else
+    echo "$fEMPTY"
+    echo "$fOK Removing ${sBGREEN}$2${sRESET}..."
+    rm "$edLI/$2"
+  fi
+}
+
+
 # View link entries.
 tLi_view_entries() {
   for file in "$edLI"/*; do
@@ -47,13 +63,13 @@ tLi_help() {
   echo "$fEMPTY"
   echo "$fUSAGE ${sHL}tLi [target1] [target2]${sRESET}"
   echo "$fBODY  where ${sBBLUE}target1${sRESET} or ${sBBLUE}target2${sRESET}"
-  echo "$fBODY  can be remote (${sHL}ssh://hostname//path/to/target/${sRESET})"
-  echo "$fBODY  or local (${sHL}path/to/target/${sRESET})."
+  echo "$fBODY  can be remote (${sHL}ssh://hostname//path/to/target${sRESET})"
+  echo "$fBODY  or local (${sHL}path/to/target${sRESET})."
   echo "$fEMPTY"
   echo "$fTIP Only one or the other (${sBBLUE}target1${sRESET} or ${sBBLUE}target2${sRESET})"
   echo "$fBODY  can be remote--you can't link two remote targets."
   echo "$fEMPTY"
-  echo "$fEXMPL ${sHL}tLi ssh://workPC//home/tastyUser/Documents/ ~/Documents/${sRESET}"
+  echo "$fEXMPL ${sHL}tLi ssh://workPC//home/tastyUser/Documents ~/Documents${sRESET}"
 }
 
 
@@ -63,6 +79,8 @@ tLi() {
     tLi_help
   elif [ "$1" = "--view" ] || [ "$1" = "-v" ] || [ -z "$1" ]; then
     tLi_view_entries
+  elif [ "$1" = "--remove" ] || [ "$1" = "-r" ]; then
+    tLi_remove_entry "$@"
   elif [ -z "$2" ]; then
     echo "$fEMPTY"
     echo "$fERROR Invalid argument."
