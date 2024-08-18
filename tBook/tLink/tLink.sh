795513e25728e034d79c1eee43c3f5a49237b4ef
piroj <p1r0j@pm.me>
2024-08-17 22:04:55 -0400

    Updated tLink add entry logic and tLink help info.

--- tBook/tLink/tLink.sh
+++ tBook/tLink/tLink.sh
@@ -21,11 +21,14 @@ done
 # Add link entry.
 tLi_add_entry() {
   echo "$fEMPTY"
+  echo "$fWARNG ${sBGREEN}tLink${sRESET} will not validate either target for you."
+  echo "$fBODY  Type carefully!"
+  echo "$fEMPTY"
   read -p "$fINPUT Choose a name: t" linkName
   if tBo_validate_name "$linkName"; then
     linkName="t$linkName"
     touch "$edLI/$linkName"
-    newLink="alias $linkName=\"unison \"$1\" \"$2\" -auto -batch\""
+    newLink="alias $linkName=\"unison \"$2\" \"$3\" -auto -batch\""
     echo "$newLink" > "$edLI/$linkName"
   fi
 }
@@ -58,10 +61,10 @@ tLi_view_entries() {
 # Link help info.
 tLi_help() {
   echo "$fEMPTY"
-  echo "$fNEUTRAL ${sBGREEN}tLink${sRESET} is used to create ${sBBLUE}unison${sRESET} aliases,"
-  echo "$fBODY  allowing you to sync two targets."
+  echo "$fNEUTRAL ${sBGREEN}tLink${sRESET} is used to add, remove, and view ${sBBLUE}unison${sRESET} aliases,"
+  echo "$fBODY  which allow you to sync local/remote targets."
   echo "$fEMPTY"
-  echo "$fUSAGE ${sHL}tLi [target1] [target2]${sRESET}"
+  echo "$fUSAGE ${sHL}tLi -a [target1] [target2]${sRESET} to add a new alias,"
   echo "$fBODY  where ${sBBLUE}target1${sRESET} or ${sBBLUE}target2${sRESET}"
   echo "$fBODY  can be remote (${sHL}ssh://hostname//path/to/target${sRESET})"
   echo "$fBODY  or local (${sHL}path/to/target${sRESET})."
@@ -69,7 +72,14 @@ tLi_help() {
   echo "$fTIP Only one or the other (${sBBLUE}target1${sRESET} or ${sBBLUE}target2${sRESET})"
   echo "$fBODY  can be remote--you can't link two remote targets."
   echo "$fEMPTY"
-  echo "$fEXMPL ${sHL}tLi ssh://workPC//home/tastyUser/Documents ~/Documents${sRESET}"
+  echo "$fEXMPL ${sHL}tLi -a ssh://workPC//home/tastyUser/Documents ~/Documents${sRESET}"
+  echo "$fEMPTY"
+  echo "$fUSAGE ${sHL}tLi -r [target]${sRESET} to remove an existing alias,"
+  echo "$fBODY  where ${sBBLUE}target${sRESET} is the full alias name."
+  echo "$fEMPTY"
+  echo "$fEXMPL ${sHL}tLi -r tWorkSync${sRESET}"
+  echo "$fEMPTY"
+  echo "$fUSAGE ${sHL}tLi -v${sRESET} to view a list of existing aliases."
 }
 
 
@@ -84,10 +94,7 @@ tLi() {
   elif [ -z "$2" ]; then
     echo "$fEMPTY"
     echo "$fERROR Invalid argument."
-  else
-    echo "$fEMPTY"
-    echo "$fWARNG ${sBGREEN}tLink${sRESET} will not validate either target for you."
-    echo "$fBODY  Type carefully!"
+  elif [ "$1" = "--add" ] || [ "$1" = "-a" ]; then
     tLi_add_entry "$@"
   fi
 }
