5f1065b5a294e3c393790dd40be4e6979d91c300
piroj <p1r0j@pm.me>
2024-08-18 00:33:07 -0400

    Added tUp and tNav help info.

--- tRaveller/tNav/tNav.sh
+++ tRaveller/tNav/tNav.sh
@@ -9,6 +9,7 @@ dB="$dN/tBack"
 # Components.
 cB="$dB/cache"
 # Aliases.
+alias tUp="tU"
 alias tBack="tB"
 alias tNav="tN"
 
@@ -28,6 +29,46 @@ tB_cache() {
 }
 
 
+# Navigate function.
+tN_navigate() {
+  tB_cache
+  cd "$1"
+}
+
+
+# Navigate help info.
+tN_help() {
+  echo "$fEMPTY"
+  echo "$fNEUTRAL ${sBBLUE}tNav${sRESET} is a collection of navigational tools,"
+  echo "$fBODY  consisting of the following:"
+  echo "$fEMPTY"
+  echo "$fUSAGE ${sHL}tN [target]${sRESET} for navigating to a target directory"
+  echo "$fBODY  (the previous working directory will be saved for use with ${sBBLUE}tB${sRESET})."
+  echo "$fEMPTY"
+  echo "$fUSAGE ${sHL}tU${sRESET} for moving up a single directory,"
+  echo "$fBODY  or ${sHL}tU [amount]${sRESET} for moving up a specified number of directories"
+  echo "$fBODY  (again, previous working directory saved for use with ${sBBLUE}tB${sRESET})."
+  echo "$fEMPTY"
+  echo "$fUSAGE ${sHL}tB${sRESET} for moving back to the previous working directory."
+}
+
+
+# tUp callable function.
+tU() {
+  if [ -z "$1" ]; then
+    tB_cache
+    cd ..
+  elif [[ "$1" =~ ^[0-9]+$ ]]; then
+    tB_cache
+    for (( i=0; i<$1; i++ )); do
+      cd ..
+    done
+  else
+    tA_invalid_argument
+  fi
+}
+
+
 # tBack callable function.
 tB() {
   if [ ! -f "$cB" ]; then
@@ -39,16 +80,11 @@ tB() {
 }
 
 
-# Navigate function.
-tN_navigate() {
-  tB_cache
-  cd "$1"
-}
-
-
 # Callable function.
 tN() {
-  if [ ! -d "$1" ]; then
+  if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+    tN_help
+  elif [ ! -d "$1" ]; then
     tA_invalid_argument
   else
     tN_navigate "$@"
