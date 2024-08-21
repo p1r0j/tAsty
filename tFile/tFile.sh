90991a06b42fd1c5960e17b5de2fccde338821d6
piroj <p1r0j@pm.me>
2024-08-21 17:49:07 -0400

    Added tMake.

--- tFile/tFile.sh
+++ tFile/tFile.sh
@@ -6,6 +6,7 @@
 
 # Aliases.
 alias tFile="tFe"
+alias tMake="tMa"
 alias tMove="tM"
 alias tCopy="tC"
 alias tDelete="tD"
@@ -22,39 +23,22 @@ tFe_help() {
   echo "$fNEUTRAL ${sBBLUE}tFile${sRESET} is a collection of file management tools,"
   echo "$fBODY  consisting of the following:"
   echo "$fEMPTY"
-  echo "$fTALK [${sBCYAN}tMove${sRESET}] Move target file/directory."
+  echo "$fTALK [${sBCYAN}tMake${sRESET}] Create new file/directory."
+  echo "$fUSE  ${sHL}tMa -[option] [name]${sRESET}"
+  echo "$fOPT  ${sHL}tMa -f [name]${sRESET} (create new file)"
+  echo "$fBODY  ${sHL}tMa -d [name]${sRESET} (create new directory)"
+  echo "$fEMPTY"
+  echo "$fNEUTRAL [${sBCYAN}tMove${sRESET}] Move target file/directory."
   echo "$fUSE  ${sHL}tM [target] [destination]${sRESET}"
   echo "$fOPT  ${sHL}tM -f${sRESET} (force move with no confirmation)"
   echo "$fEMPTY"
-  echo "$fNEUTRAL [${sBCYAN}tCopy${sRESET}] Copy target file/directory."
+  echo "$fTALK [${sBCYAN}tCopy${sRESET}] Copy target file/directory."
   echo "$fUSE  ${sHL}tC [target] [destination]${sRESET}"
   echo "$fOPT  ${sHL}tC -f${sRESET} (force copy with no confirmation)"
   echo "$fEMPTY"
-  echo "$fTALK [${sBCYAN}tDelete${sRESET}] Delete target file/directory."
+  echo "$fNEUTRAL [${sBCYAN}tDelete${sRESET}] Delete target file/directory."
   echo "$fUSE  ${sHL}tD [target]${sRESET}"
   echo "$fOPT  ${sHL}tD -f${sRESET} (force deletion with no confirmation)"
-  echo "$fEMPTY"
-  echo "$fNEUTRAL [${sBCYAN}tWrite${sRESET}] Write to target file."
-  echo "$fUSE  ${sHL}tW "[string]" [target]${sRESET} (append string to target)"
-  echo "$fOPT  ${sHL}tW -r${sRESET} (replace target with string)"
-  echo "$fEMPTY"
-  echo "$fTIP ${sBBLUE}tWrite${sRESET} can be used to create new files"
-  echo "$fBODY  using the ${sBBLUE}replace${sRESET} option."
-  echo "$fBODY  With this option, the target will be"
-  echo "$fBODY  created if it does not already exist."
-  echo "$fEMPTY"
-  echo "$fTALK [${sBCYAN}tEdit${sRESET}] Open file for editing."
-  echo "$fUSE  ${sHL}tE [target]${sRESET}"
-  echo "$fEMPTY"
-  echo "$fTIP ${sBBLUE}tEdit${sRESET} uses your preferred ${sBBLUE}EDITOR${sRESET}"
-  echo "$fBODY  (generally defined in your ${sHL}~/.bashrc${sRESET} file)."
-  echo "$fBODY  Otherwise, it will default to ${sBBLUE}vim${sRESET}."
-  echo "$fEMPTY"
-  echo "$fNEUTRAL [${sBCYAN}tReplace${sRESET}] Replace specified strings in a target file."
-  echo "$fUSE  ${sHL}tRp [target] \"[search phrase]\" \"[replacement phrase]\"${sRESET}"
-  echo "$fEMPTY"
-  echo "$fTIP ${sBBLUE}tReplace${sRESET} will ignore lines containing the safeword,"
-  echo "$fBODY  ${sHL}wHiskey${sRESET} (caps sensitive)."
   echo "$fEMPTY"
   echo "$fTALK [${sBCYAN}tSave${sRESET}] Save copy of target using ${sBBLUE}rsync${sRESET}."
   echo "$fUSE  ${sHL}tSv [target]${sRESET} (save copy as [target].save)"
@@ -63,6 +47,28 @@ tFe_help() {
   echo "$fNEUTRAL [${sBCYAN}tLoad${sRESET}] Load saved copy of target using ${sBBLUE}rsync${sRESET}."
   echo "$fUSE  ${sHL}tL [target]${sRESET} (load [target].save as [target])"
   echo "$fBODY  ${sHL}tL [target] [name]${sRESET} (load [target].save as [name])"
+  echo "$fEMPTY"
+  echo "$fTALK [${sBCYAN}tWrite${sRESET}] Write to target file."
+  echo "$fUSE  ${sHL}tW "[string]" [target]${sRESET} (append string to target)"
+  echo "$fOPT  ${sHL}tW -r${sRESET} (replace target with string)"
+  echo "$fEMPTY"
+  echo "$fTIP ${sBBLUE}tWrite${sRESET} can be used to create new files"
+  echo "$fBODY  using the ${sBBLUE}replace${sRESET} option."
+  echo "$fBODY  With this option, the target will be"
+  echo "$fBODY  created if it does not already exist."
+  echo "$fEMPTY"
+  echo "$fNEUTRAL [${sBCYAN}tEdit${sRESET}] Open file for editing."
+  echo "$fUSE  ${sHL}tE [target]${sRESET}"
+  echo "$fEMPTY"
+  echo "$fTIP ${sBBLUE}tEdit${sRESET} uses your preferred ${sBBLUE}EDITOR${sRESET}"
+  echo "$fBODY  (generally defined in your ${sHL}~/.bashrc${sRESET} file)."
+  echo "$fBODY  Otherwise, it will default to ${sBBLUE}vim${sRESET}."
+  echo "$fEMPTY"
+  echo "$fTALK [${sBCYAN}tReplace${sRESET}] Replace specified strings in a target file."
+  echo "$fUSE  ${sHL}tRp [target] \"[search phrase]\" \"[replacement phrase]\"${sRESET}"
+  echo "$fEMPTY"
+  echo "$fTIP ${sBBLUE}tReplace${sRESET} will ignore lines containing the safeword,"
+  echo "$fBODY  ${sHL}wHiskey${sRESET} (caps sensitive)."
 }
 
 
@@ -261,6 +267,30 @@ tM() {
     fi
   elif [ -f "$1" ] || [ -d "$1" ]; then
     mv "$1" "$2"
+  else
+    tA_invalid_argument
+  fi
+}
+
+
+# tMake callable function.
+tMa() {
+  if [ -z "$2" ]; then
+    tA_too_few_arguments
+  elif [ "$1" = "--file" ] || [ "$1" = "-f" ]; then
+    if [ ! -f "$2" ] && [ ! -d "$2" ]; then
+      touch "$2"
+    else
+      echo "$fEMPTY"
+      echo "$fERROR ${sHL}$2${sRESET} already exists."
+    fi
+  elif [ "$1" = "--directory" ] || [ "$1" = "-d" ]; then
+    if [ ! -f "$2" ] && [ ! -d "$2" ]; then
+      mkdir "$2"
+    else
+      echo "$fEMPTY"
+      echo "$fERROR ${sHL}$2${sRESET} already exists."
+    fi
   else
     tA_invalid_argument
   fi
